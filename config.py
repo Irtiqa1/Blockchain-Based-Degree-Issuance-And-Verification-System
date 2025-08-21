4eb3595bd55875c2da8e5cd8553fb5c79e2cf928
Irtiqa1 <irtiqabukhari404@gmail.com>
2025-08-21 16:57:36 +0530

    Removed dotenv load for production

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '.env'))
+# load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your-very-secret-key-123')
